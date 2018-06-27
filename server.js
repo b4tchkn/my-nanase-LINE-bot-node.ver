13bfbddbbc496ce722403ffbca1b6b8cb9cdbd28
kakeru_nakabachi <b1015146@fun.ac.jp>
2018-06-27 16:23:12 +0900

    [add]日付返答

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 const express = require('express');
 const line = require('@line/bot-sdk');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const config = {
-  channelAccessToken: process.env.ACESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
+  channelAccessToken: "1Jm0jNA9ovgwCyD9MMEQT1P8R+nsaf+uzPiZKF0NcHbGlS6qOQWC7Xzszy4d1q2MpaDqoMoYKzJ4hSNk7V0qXxmxCQNpHqJ6Ouro+0hmJR4pvFp6fqnPEpIW4zfMclRFHeVK/WHE1bja+XVrxIq+RAdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "fe5a2cbeb9c5cfa20434ff6691ddc642"
 };
 
 const app = express();
@@ -25,11 +25,37 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
+  let repMes = '';
+  let sendMes = event.message.text;
+
+  if (/おはよ/.test(sendMes)) {
+    let date = getDate();
+
+    repMes = `おはよう！\n` +
+      `今日は ${date} だよ！\n` +
+      `今日は何するの？`;
+  }
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
+    text: repMes //実際に返信の言葉を入れる箇所
   });
 }
 
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
+
+// 現在の日付取得
+const getDate = () => {
+  let today = new Date();
+
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+  let week = today.getDay();
+
+  let weekDay = new Array("日", "月", "火", "水", "木", "金", "土");
+
+  let formatDate = `${month}/${day} ${weekDay[week]}曜日`;
+
+  return formatDate;
+}
